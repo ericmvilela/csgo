e5528b2806bd657ce6901a01185c78e4a7679a9a
Eric <ericmvilela@hotmail.com>
2022-05-10 21:28:22 -0300

    first commit

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--hirq^2-p@_whsuta(ugt_%3^o5ajb7^9g%h%c&=jcx!)%-&m!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
